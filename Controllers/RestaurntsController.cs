33826f31c573a4a174005e20f881359672f18101
Harold_Mesa <harold.mesa@icloud.com>
2022-10-06 12:16:40 -0500

    Add RestaurantsController.cs
    Co-authored-by: Kelly Bruce <kelly.bruce.cs@gmail.com>
    Co-authored-by: Trevor Hunter <trevorh8700@gmail.com>

--- Controllers/RestaurntsController.cs
+++ Controllers/RestaurntsController.cs
@@ -22,5 +22,54 @@ namespace BestRestaurants.Controllers
       return View(model); 
     }
 
+    public ActionResult Create()
+    {
+      ViewBag.CuisineId = new SelectList(_db.TypeOfCuisine, "CuisineId", "TypeOfCuisine");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Restaurant restaurant)
+    {
+      _db.Restaurant.Add(restaurant);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Restaurant thisRestaurant = _db.Restaurants.FirstOrDefault(restaurant => restaurant.RestaurantId == id);
+      return View(thisRestaurant);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisRestaurant = _db.Restaurants.FirstOrDefault(restaurant => restaurant.RestaurantId == id);
+      ViewBag.CuisineId = new SelectList(_db.TypeOfCuisine, "CuisineId", "Name");
+      return View(thisRestaurant);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Restaurant restaurant)
+    {
+      _db.Entry(restaurant).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisRestaurant = _db.Restaurants.FirstOrDefault(restaurant => restaurant.RestaurantId == id);
+      return View(thisRestaurant);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisRestaurant = _db.Restaurants.FirstOrDefault(restaurant => restaurant.RestaurantId == id);
+      _db.Restaurants.Remove(thisRestaurant);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
